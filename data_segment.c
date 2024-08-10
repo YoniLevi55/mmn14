53eb4ddfbed9e83bdf80776189e1d6fc1482aa38
YoniLevi55 <sasi@iguaz.io>
2024-08-10 21:04:23 +0300

    added comments

--- data_segment.c
+++ data_segment.c
@@ -6,33 +6,34 @@ int dataSegmentSize = 0;
 int DC = 0;
 
 
-unsigned short* get_data_segment()
+unsigned short* get_data_segment() //get data segment
 {
     return dataSegment;
 }
 
-void init_data_segment(int size)
+void init_data_segment(int size) //init data segment
 {
     dataSegment = (unsigned short*)malloc(sizeof(unsigned short) * size);
 }
 
-int get_DC()
+int get_DC() //get DC
 {
     return DC;
 }
 
-int get_data_segment_size()
+int get_data_segment_size() //get data segment size
 {
     return dataSegmentSize;
 }
 
-void dataSegment_add_data(unsigned short value)
+void dataSegment_add_data(unsigned short value) //add data to data segment
 {
-    logger(DEBUG,"dataSegment:: Adding data: %d\n",value);
-    if (dataSegment == NULL)
+    if (dataSegment == NULL) //if data segment is null
     {
         init_data_segment(1);
-    } else {
+    }
+    else //if data segment is not null
+    {
         dataSegment = (unsigned short*)realloc(dataSegment, sizeof(unsigned short) * (DC+1));
     }
     dataSegment[DC] = value;
